79263672f50b7aa31cf61bfdb5adb50f5fb4627a
Glenn Hope <glenn.alexander.hope@gmail.com>
2020-08-02 21:17:13 -0700

    Run :Format

--- library/std/src/sys/psp/stdio.rs
+++ library/std/src/sys/psp/stdio.rs
@@ -15,11 +15,7 @@ impl io::Read for Stdin {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         unsafe {
             let fd = sceKernelStdin();
-            let ret = sceIoRead(
-                fd,
-                buf.as_ptr() as _,
-                buf.len() as _,
-            );
+            let ret = sceIoRead(fd, buf.as_ptr() as _, buf.len() as _);
             Ok(ret as usize)
         }
     }
@@ -35,11 +31,7 @@ impl io::Write for Stdout {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         unsafe {
             let fd = sceKernelStdout();
-            let ret = sceIoWrite(
-                fd,
-                buf.as_ptr() as _,
-                buf.len(),
-            );
+            let ret = sceIoWrite(fd, buf.as_ptr() as _, buf.len());
         }
         Ok(buf.len())
     }
@@ -59,11 +51,7 @@ impl io::Write for Stderr {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         unsafe {
             let fd = sceKernelStderr();
-            let ret = sceIoWrite(
-                fd,
-                buf.as_ptr() as _,
-                buf.len(),
-            );
+            let ret = sceIoWrite(fd, buf.as_ptr() as _, buf.len());
         }
         Ok(buf.len())
     }
