69836d696e8d8cfdae6b3a6d1c2bb11ef41c9709
Paul Sajna <sajattack@gmail.com>
2020-08-14 17:01:11 -0700

    wip net

--- library/std/src/sys/psp/net.rs
+++ library/std/src/sys/psp/net.rs
@@ -9,6 +9,8 @@ pub struct TcpStream(Void);
 
 impl TcpStream {
     pub fn connect(_: io::Result<&SocketAddr>) -> io::Result<TcpStream> {
+        //libc::sceInetSocket(netc::AF_INET, 
+        //libc::sceInetConnect
         unsupported()
     }
 
@@ -324,9 +326,51 @@ impl<'a> TryFrom<(&'a str, u16)> for LookupHost {
 
 #[allow(nonstandard_style)]
 pub mod netc {
-    pub const AF_INET: u8 = 0;
-    pub const AF_INET6: u8 = 1;
+    pub const AF_UNSPEC: u8 = 0;
+    pub const AF_LOCAL: u8 = 1;
+    pub const AF_UNIX: u8 = AF_LOCAL;
+    pub const AF_INET: u8 = 2;
+    pub const AF_IMPLINK: u8 = 3;
+    pub const AF_PUP: u8 = 4;
+    pub const AF_CHAOS: u8 = 5;
+    pub const AF_NS: u8 = 6;
+    pub const AF_ISO: u8 = 7;
+    pub const AF_OSI: u8 = AF_ISO;
+    pub const AF_ECMA: u8 = 8;
+    pub const AF_DATAKIT: u8 = 9;
+    pub const AF_CCITT: u8 = 10;
+    pub const AF_SNA: u8 = 11;
+    pub const AF_DECnet: u8 = 12;
+    pub const AF_DLI: u8 = 13;
+    pub const AF_LAT: u8 = 14;
+    pub const AF_HYLINK: u8 = 15;
+    pub const AF_APPLETALK: u8 = 16;
+    pub const AF_ROUTE: u8 = 17;
+    pub const AF_LINK: u8 = 18;
+    pub const AF_COIP: u8 = 20;
+    pub const AF_CNT: u8 = 21;
+    pub const AF_IPX: u8 = 23;
+    pub const AF_INET6: u8 = 24;
+    pub const AF_ISDN: u8 = 26;
+    pub const AF_E164: u8 = AF_ISDN;
+    pub const AF_NATM: u8 = 27;
+    pub const AF_ARP: u8 = 28;
+    pub const AF_MAX: u8 = 31;
+
+    pub const SOCK_STREAM: i32 = 1;
+    pub const SOCK_DGRAM: i32 = 2;
+    pub const SOCK_RAW: i32 = 3;
+    pub const SOCK_RDM: i32 = 4;
+    pub const SOCK_SEQPACKET: i32 = 5;
     pub type sa_family_t = u8;
+
+    //pub use libc::in_addr;
+
+    //pub use libc::sockaddr as sockaddr_in;
+
+    //pub use libc::sockaddr;
+
+    //pub use libc::socklen_t;
 
     #[derive(Copy, Clone)]
     pub struct in_addr {
@@ -340,19 +384,19 @@ pub mod netc {
         pub sin_addr: in_addr,
     }
 
-    #[derive(Copy, Clone)]
-    pub struct in6_addr {
-        pub s6_addr: [u8; 16],
-    }
+    //#[derive(Copy, Clone)]
+    //pub struct in6_addr {
+        //pub s6_addr: [u8; 16],
+    //}
 
-    #[derive(Copy, Clone)]
-    pub struct sockaddr_in6 {
-        pub sin6_family: sa_family_t,
-        pub sin6_port: u16,
-        pub sin6_addr: in6_addr,
-        pub sin6_flowinfo: u32,
-        pub sin6_scope_id: u32,
-    }
+    //#[derive(Copy, Clone)]
+    //pub struct sockaddr_in6 {
+        //pub sin6_family: sa_family_t,
+        //pub sin6_port: u16,
+        //pub sin6_addr: in6_addr,
+        //pub sin6_flowinfo: u32,
+        //pub sin6_scope_id: u32,
+    //}
 
     #[derive(Copy, Clone)]
     pub struct sockaddr {}
