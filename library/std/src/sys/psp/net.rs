e339531cc4ef44f4d777254572693d044c7fab8d
Paul Sajna <sajattack@gmail.com>
2020-09-17 21:20:12 -0700

    tcp and dns

--- library/std/src/sys/psp/net.rs
+++ library/std/src/sys/psp/net.rs
@@ -1,17 +1,48 @@
 use crate::convert::TryFrom;
 use crate::fmt;
 use crate::io::{self, IoSlice, IoSliceMut};
-use crate::net::{Ipv4Addr, Ipv6Addr, Shutdown, SocketAddr};
+use crate::net::{Ipv4Addr, Ipv6Addr, Shutdown, SocketAddr, SocketAddrV4};
 use crate::sys::{unsupported, Void};
+use crate::sys_common::IntoInner;
 use crate::time::Duration;
+use crate::ffi::CString;
 
-pub struct TcpStream(Void);
+use core::ffi::c_void;
+
+pub struct Socket(i32);
+
+impl Drop for Socket {
+    fn drop(&mut self) {
+        unsafe { libc::sceNetInetClose(self.0); }
+    }
+}
+
+pub struct TcpStream {
+    socket: Socket,
+    peer_addr: SocketAddrV4,
+}
 
 impl TcpStream {
-    pub fn connect(_: io::Result<&SocketAddr>) -> io::Result<TcpStream> {
-        //libc::sceInetSocket(netc::AF_INET, 
-        //libc::sceInetConnect
-        unsupported()
+    pub fn connect(addr: io::Result<&SocketAddr>) -> io::Result<TcpStream> {
+        let sock = unsafe { libc::sceNetInetSocket(netc::AF_INET as i32, netc::SOCK_STREAM, 0) };
+        if sock < 0 {
+            todo!()
+        } else {
+            let addr = addr?;
+            match addr {
+                SocketAddr::V4(v4) => {
+                    let (addr, len) = addr.into_inner();
+                    if unsafe { libc::sceNetInetConnect(sock, addr, len as u32) } < 0  {
+                        todo!("Error Handling")
+                    } else {
+                        Ok(TcpStream{socket: Socket(sock), peer_addr: *v4})
+                    }
+                }
+                SocketAddr::V6(_) => {
+                    unsupported()
+                }
+            }
+        }
     }
 
     pub fn connect_timeout(_: &SocketAddr, _: Duration) -> io::Result<TcpStream> {
@@ -19,143 +50,192 @@ impl TcpStream {
     }
 
     pub fn set_read_timeout(&self, _: Option<Duration>) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn set_write_timeout(&self, _: Option<Duration>) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn read_timeout(&self) -> io::Result<Option<Duration>> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn write_timeout(&self) -> io::Result<Option<Duration>> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn peek(&self, _: &mut [u8]) -> io::Result<usize> {
-        match self.0 {}
+        unsupported()
     }
 
-    pub fn read(&self, _: &mut [u8]) -> io::Result<usize> {
-        match self.0 {}
+    pub fn read(&self, buf: &mut [u8]) -> io::Result<usize> {
+        let result = unsafe { libc::sceNetInetRecv(self.socket.0, buf.as_mut_ptr() as *mut c_void, buf.len(), 0) };
+        if result < 0 {
+            let err = unsafe { libc::sceNetInetGetErrno() };
+            Err(io::Error::new(io::ErrorKind::Other, err.to_string()))    
+        } else {
+            Ok(result)
+        }
     }
 
     pub fn read_vectored(&self, _: &mut [IoSliceMut<'_>]) -> io::Result<usize> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn is_read_vectored(&self) -> bool {
-        match self.0 {}
+        false
     }
 
-    pub fn write(&self, _: &[u8]) -> io::Result<usize> {
-        match self.0 {}
+    pub fn write(&self, buf: &[u8]) -> io::Result<usize> {
+        let result = unsafe { libc::sceNetInetSend(self.socket.0, buf.as_ptr() as *const c_void, buf.len(), 0) };
+        if result < 0 {
+            todo!("Error Handling")
+        } else {
+            Ok(result)
+        }
     }
 
     pub fn write_vectored(&self, _: &[IoSlice<'_>]) -> io::Result<usize> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn is_write_vectored(&self) -> bool {
-        match self.0 {}
+        false
     }
 
     pub fn peer_addr(&self) -> io::Result<SocketAddr> {
-        match self.0 {}
+        Ok(SocketAddr::V4(self.peer_addr))
     }
 
     pub fn socket_addr(&self) -> io::Result<SocketAddr> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn shutdown(&self, _: Shutdown) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn duplicate(&self) -> io::Result<TcpStream> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn set_nodelay(&self, _: bool) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn nodelay(&self) -> io::Result<bool> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn set_ttl(&self, _: u32) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn ttl(&self) -> io::Result<u32> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn take_error(&self) -> io::Result<Option<io::Error>> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn set_nonblocking(&self, _: bool) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 }
 
 impl fmt::Debug for TcpStream {
-    fn fmt(&self, _f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self.0 {}
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("unsupported")
     }
 }
 
-pub struct TcpListener(Void);
+pub struct TcpListener(Socket);
 
 impl TcpListener {
-    pub fn bind(_: io::Result<&SocketAddr>) -> io::Result<TcpListener> {
-        unsupported()
+    pub fn bind(addr: io::Result<&SocketAddr>) -> io::Result<TcpListener> {
+        let sock = unsafe { libc::sceNetInetSocket(netc::AF_INET as i32, netc::SOCK_STREAM, 0) };
+        if sock < 0 {
+            todo!()
+        } else {
+            let addr = addr?;
+            match addr {
+                SocketAddr::V4(v4) => {
+                    let (addr, len) = addr.into_inner();
+                    if unsafe { libc::sceNetInetBind(sock, addr, len as u32) } < 0  {
+                        todo!("Error Handling")
+                    } else {
+                        if unsafe { libc::sceNetInetListen(sock, 128) } < 0 {
+                            todo!("Error Handling")
+                        } else {
+                            Ok(TcpListener(Socket(sock)))
+                        }
+                    }
+                }
+                SocketAddr::V6(_) => {
+                    unsupported()
+                }
+            }
+        }
     }
 
     pub fn socket_addr(&self) -> io::Result<SocketAddr> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn accept(&self) -> io::Result<(TcpStream, SocketAddr)> {
-        match self.0 {}
+        let mut addr: netc::sockaddr = unsafe { core::mem::zeroed() };
+        let mut addr_len: netc::socklen_t = 0; 
+        let sock = unsafe { libc::sceNetInetAccept(self.0.0, &mut addr, &mut addr_len) };
+        if sock < 0 {
+            todo!("Error Handling")
+        } else {
+            let addr = unsafe { core::mem::transmute::<netc::sockaddr, netc::sockaddr_in>(addr) };
+            let port = addr.sin_port;
+            let octets = u32::to_le_bytes(addr.sin_addr.s_addr);
+            let sockaddr = SocketAddrV4::new(Ipv4Addr::new(octets[0], octets[1], octets[2], octets[3]), port);
+            let stream = TcpStream {
+                socket: Socket(sock),
+                peer_addr: sockaddr,
+            };
+            Ok((stream, SocketAddr::V4(sockaddr)))
+        }
+        unsupported()
     }
 
     pub fn duplicate(&self) -> io::Result<TcpListener> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn set_ttl(&self, _: u32) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn ttl(&self) -> io::Result<u32> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn set_only_v6(&self, _: bool) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn only_v6(&self) -> io::Result<bool> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn take_error(&self) -> io::Result<Option<io::Error>> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn set_nonblocking(&self, _: bool) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 }
 
 impl fmt::Debug for TcpListener {
-    fn fmt(&self, _f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self.0 {}
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("unsupported")
     }
 }
 
@@ -167,160 +247,223 @@ impl UdpSocket {
     }
 
     pub fn peer_addr(&self) -> io::Result<SocketAddr> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn socket_addr(&self) -> io::Result<SocketAddr> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn recv_from(&self, _: &mut [u8]) -> io::Result<(usize, SocketAddr)> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn peek_from(&self, _: &mut [u8]) -> io::Result<(usize, SocketAddr)> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn send_to(&self, _: &[u8], _: &SocketAddr) -> io::Result<usize> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn duplicate(&self) -> io::Result<UdpSocket> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn set_read_timeout(&self, _: Option<Duration>) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn set_write_timeout(&self, _: Option<Duration>) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn read_timeout(&self) -> io::Result<Option<Duration>> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn write_timeout(&self) -> io::Result<Option<Duration>> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn set_broadcast(&self, _: bool) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn broadcast(&self) -> io::Result<bool> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn set_multicast_loop_v4(&self, _: bool) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn multicast_loop_v4(&self) -> io::Result<bool> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn set_multicast_ttl_v4(&self, _: u32) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn multicast_ttl_v4(&self) -> io::Result<u32> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn set_multicast_loop_v6(&self, _: bool) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn multicast_loop_v6(&self) -> io::Result<bool> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn join_multicast_v4(&self, _: &Ipv4Addr, _: &Ipv4Addr) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn join_multicast_v6(&self, _: &Ipv6Addr, _: u32) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn leave_multicast_v4(&self, _: &Ipv4Addr, _: &Ipv4Addr) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn leave_multicast_v6(&self, _: &Ipv6Addr, _: u32) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn set_ttl(&self, _: u32) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn ttl(&self) -> io::Result<u32> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn take_error(&self) -> io::Result<Option<io::Error>> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn set_nonblocking(&self, _: bool) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn recv(&self, _: &mut [u8]) -> io::Result<usize> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn peek(&self, _: &mut [u8]) -> io::Result<usize> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn send(&self, _: &[u8]) -> io::Result<usize> {
-        match self.0 {}
+        unsupported()
     }
 
     pub fn connect(&self, _: io::Result<&SocketAddr>) -> io::Result<()> {
-        match self.0 {}
+        unsupported()
     }
 }
 
 impl fmt::Debug for UdpSocket {
     fn fmt(&self, _f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self.0 {}
+        match self.0 {} 
     }
 }
 
-pub struct LookupHost(Void);
+pub struct LookupHost {
+    hostname: CString,
+    port: u16,
+    resolver_id: i32,
+    resolver_buf: [u8; 1024],
+    timeout: u32,
+    retries: i32,
+    // TODO is this necessary? Just don't want to get caught in an infinite loop
+    // returning the same address with no errors returned from sceNetResolverStartNtoA
+    prev_result: u32,
+}
 
 impl LookupHost {
     pub fn port(&self) -> u16 {
-        match self.0 {}
+        self.port
     }
 }
 
 impl Iterator for LookupHost {
     type Item = SocketAddr;
     fn next(&mut self) -> Option<SocketAddr> {
-        match self.0 {}
+        let mut in_addr: libc::in_addr = unsafe { core::mem::zeroed() };
+        let result =  unsafe { libc::sceNetResolverStartNtoA(self.resolver_id, self.hostname.as_ptr() as *const u8, &mut in_addr, self.timeout, self.retries) };
+        if result < 0 || in_addr.0 == self.prev_result {
+            None
+        } else {
+            self.prev_result = in_addr.0;
+            let octets = u32::to_le_bytes(in_addr.0);
+            Some(SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(octets[0], octets[1], octets[2], octets[3]), self.port)))
+        }
     }
 }
 
+//TODO less unwrapping
 impl TryFrom<&str> for LookupHost {
     type Error = io::Error;
 
-    fn try_from(_v: &str) -> io::Result<LookupHost> {
-        unsupported()
+    fn try_from(v: &str) -> io::Result<LookupHost> {
+        let mut split = v.split(":");
+        let host = split.next().unwrap();
+        let mut port: u16 = 80;
+        let next = split.next();
+        if next.is_some() {
+            port = next.unwrap().parse::<u16>().unwrap();
+        }
+        let cstring = crate::ffi::CString::new(host).unwrap();
+        
+        let mut rid: i32 = 0;
+        let mut dns_buf: [u8; 1024] = [0u8; 1024];
+        if unsafe { libc::sceNetResolverCreate(&mut rid, &mut dns_buf[0] as *mut _ as *mut _, dns_buf.len() as u32) } < 0 {
+            todo!("Error Handling");
+        } else {
+            Ok(LookupHost {
+                hostname: cstring,
+                port,
+                resolver_id: rid,
+                resolver_buf: dns_buf,
+                retries: 5,
+                timeout: 5,
+                prev_result: 0,
+            })
+        }
     }
 }
 
-impl<'a> TryFrom<(&'a str, u16)> for LookupHost {
+impl TryFrom<(&str, u16)> for LookupHost {
     type Error = io::Error;
 
-    fn try_from(_v: (&'a str, u16)) -> io::Result<LookupHost> {
-        unsupported()
+    fn try_from(v: (&str, u16)) -> io::Result<LookupHost> {
+        let cstring = crate::ffi::CString::new(v.0).unwrap();
+        let mut rid: i32 = 0;
+        let mut dns_buf: [u8; 1024] = [0u8; 1024];
+        if unsafe { libc::sceNetResolverCreate(&mut rid, &mut dns_buf[0] as *mut _ as *mut _, dns_buf.len() as u32) } < 0 {
+            todo!("Error Handling");
+        } else {
+            Ok(LookupHost {
+                hostname: cstring,
+                port: v.1,
+                resolver_id: rid,
+                resolver_buf: dns_buf,
+                retries: 5,
+                timeout: 5,
+                prev_result: 0,
+            })
+        }    
+    }
+}
+
+impl Drop for LookupHost {
+    fn drop(&mut self) {
+        unsafe { libc::sceNetResolverDelete(self.resolver_id) };
     }
 }
 
@@ -364,42 +507,33 @@ pub mod netc {
     pub const SOCK_SEQPACKET: i32 = 5;
     pub type sa_family_t = u8;
 
-    //pub use libc::in_addr;
+    pub use libc::in_addr;
 
-    //pub use libc::sockaddr as sockaddr_in;
+    pub use libc::sockaddr;
 
-    //pub use libc::sockaddr;
+    pub use libc::socklen_t;
 
-    //pub use libc::socklen_t;
-
-    #[derive(Copy, Clone)]
-    pub struct in_addr {
-        pub s_addr: u32,
-    }
-
+    #[repr(C)]
     #[derive(Copy, Clone)]
     pub struct sockaddr_in {
-        pub sin_family: sa_family_t,
+        pub sin_len: u8,
+        pub sin_family: u8,
         pub sin_port: u16,
         pub sin_addr: in_addr,
+        pub sin_zero: [u8; 8]
     }
 
-    //#[derive(Copy, Clone)]
-    //pub struct in6_addr {
-        //pub s6_addr: [u8; 16],
-    //}
-
-    //#[derive(Copy, Clone)]
-    //pub struct sockaddr_in6 {
-        //pub sin6_family: sa_family_t,
-        //pub sin6_port: u16,
-        //pub sin6_addr: in6_addr,
-        //pub sin6_flowinfo: u32,
-        //pub sin6_scope_id: u32,
-    //}
+    #[derive(Copy, Clone)]
+    pub struct in6_addr {
+        pub s6_addr: [u8; 16],
+    }
 
     #[derive(Copy, Clone)]
-    pub struct sockaddr {}
-
-    pub type socklen_t = usize;
+    pub struct sockaddr_in6 {
+        pub sin6_family: sa_family_t,
+        pub sin6_port: u16,
+        pub sin6_addr: in6_addr,
+        pub sin6_flowinfo: u32,
+        pub sin6_scope_id: u32,
+    }
 }
